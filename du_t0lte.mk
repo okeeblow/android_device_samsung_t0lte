e788bab73f7717697b12968e8f83eb897ccdd915
Bret Zamzow <mazwoz@gmail.com>
2013-12-27 21:38:41 -0800

    Updated to DU

--- du_t0lte.mk
+++ du_t0lte.mk
@@ -13,16 +13,16 @@
 # limitations under the License.
 
 # Inherit Omni GSM telephony parts
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/du/config/gsm.mk)
 
 # Inherit from our omni product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/du/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/t0lte/full_t0lte.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := omni_t0lte
+PRODUCT_NAME := du_t0lte
 PRODUCT_DEVICE := t0lte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
